fec99ea702d50e5018447cf1e1f915123853c120
Naomie Pont <56826761+npont@users.noreply.github.com>
2025-11-24 17:17:04 +0100

    Add files via upload

---
+++ DCM_motifs_in_Genome/Danio_rerio/Ensembl/job.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+#SBATCH -J fastaregexfinder
+#SBATCH --mem=2000
+#SBATCH -t 2:00:00
+#SBATCH --mail-type=BEGIN,END
+#SBATCH -o output_fastaregexfinder
+#SBATCH -e error_fastaregexfinder
+
+genome='/path_to_Reference/Danio_rerio.GRCz11.dna.primary_assembly.fa'
+
+python3 ../../fastaRegexFinder.py -f $genome -r 'CCAGG' > CCAGG_motifs.bed
+
+python3 ../../fastaRegexFinder.py -f $genome -r 'CCTGG' > CCTGG_motifs.bed
+
+cat CCAGG_motifs.bed CCTGG_motifs.bed > tmp.bed
+
+awk 'BEGIN{OFS="\t"} {print $1, $2, $3, $4, $6, $7}' tmp.bed > tmp_.bed
+
+bedtools sort -i tmp_.bed > dcm_motifs_zero_based.bed
+
+awk 'BEGIN{OFS="\t"} {$2=$2+1; print}'  dcm_motifs_zero_based.bed >  dcm_motifs_one_based.bed
+
+rm tmp.bed tmp_.bed
